b14ceb72d8a6ff3ebd490b5f60df9ec89b291355
Meukieje <meukiejeroni05@icloud.com>
2025-01-24 14:10:56 +0100

    streamlit_app2.py

--- streamlit_app2.py
+++ streamlit_app2.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 import altair as alt
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
