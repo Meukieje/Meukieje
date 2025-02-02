05d061d048ac9bc2c1df3837ac3fdfaf4e73b7ca
Meukieje <meukiejeroni05@icloud.com>
2025-02-02 21:41:22 +0100

    bankMeukiejestreamlit_app.py

--- bankMeukiejestreamlit_app.py
+++ bankMeukiejestreamlit_app.py
@@ -14,10 +14,3 @@ age = st.number_input("Âge", min_value=18, max_value=100, value=30)
 job = st.selectbox("Job", ["admin.", "blue-collar", "entrepreneur", "housemaid", "management", "retired", "self-employed", "services", "student", "technician", "unemployed", "unknown"])
 marital = st.selectbox("Statut marital", ["divorced", "married", "single", "unknown"])
 education = st.selectbox("Éducation", ["basic.4y", "basic.6y", "basic.9y", "high.school", "illiterate", "professional.course", "university.degree", "unknown"])
-git clone https://github.com/Meukieje/bankAddiMeukieje.git
-cd bankAddiMeukieje
-mv ../bank_model.pkl .
-mv ../app.py .
-git add .
-git commit -m "Ajout du projet Streamlit"
-git push origin main
